20d8e420a12d7f1652d04080c71f4070a1b9f9bd
Vivian Trieu <v.t.trieu98@gmail.com>
2019-05-11 14:09:10 -0400

    python linked list

--- LL.py
+++ LL.py
@@ -20,24 +20,24 @@ class LL(object):
 	def __init__ (self, head = None):
 		self.head = head
 		
-#Insert Method
-def insert(self, data):
-	node = Node(data)
-	node.setNext(self.head)
-	self.head = node
-	
-#Delete Method
-def delete(self, data):
-	ptr = self.head
-	prev = None
-	while ptr is not None:
-		if ptr.getData == data:
-			prev.setNext(ptr.getNext())
-			break
-		else:
-			prev = ptr
-			ptr = ptr.getNext()
-	if prev is None:
-		self.head = ptr.getNext()
-	if ptr is None:
-		print "Data is not in linked list."
+	#Insert Method
+	def insert(self, data):
+		node = Node(data)
+		node.setNext(self.head)
+		self.head = node
+		
+	#Delete Method
+	def delete(self, data):
+		ptr = self.head
+		prev = None
+		while ptr is not None:
+			if ptr.getData == data:
+				prev.setNext(ptr.getNext())
+				break
+			else:
+				prev = ptr
+				ptr = ptr.getNext()
+		if prev is None:
+			self.head = ptr.getNext()
+		if ptr is None:
+			print "Data is not in linked list."
