eb602dc3e535e56bb90054cb7811d42d15bc9d52
Vivian Trieu <v-trieu@github.com>
2019-05-11 22:32:38 -0400

    heroku testing

--- webpage.py
+++ webpage.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -32,4 +33,5 @@ def data_structure():
 	return output
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port = 5000)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port = port)
